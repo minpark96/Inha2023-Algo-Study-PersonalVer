acf9ff87b66b16883eb18f247140213a58f70c61
minpark96 <hmpark3355@gmail.com>
2023-07-24 21:06:11 +0900

    00-04. Done

--- 0-4_Clone_Graph.cpp
+++ 0-4_Clone_Graph.cpp
@@ -1,6 +1,4 @@
-#include <vector>
-using std::vector;
-
+/*
 // Definition for a Node.
 class Node {
 public:
@@ -19,7 +17,7 @@ public:
         neighbors = _neighbors;
     }
 };
-
+*/
 
 class Solution {
 public:
@@ -27,54 +25,42 @@ public:
         if (node == nullptr)
             return nullptr;
 
-        Node* origin = new Node(1);
+        Node* root = new Node(1);
+
         if (!node->neighbors.empty())
         {
-            // 첫번째 노드랑 마지막 노드가 반드시 연결되어있는지;
-            // 간선 수가 첫번째가 가장 많은지 모르겠음;;;;
-            int numEdge = node->neighbors.size();
-            int endVal = node->neighbors[numEdge - 1]->val;
-
-            Node** nodeList = new Node * [endVal];
-            nodeList[0] = origin;
-            for (int i = 1; i < endVal; i++)
+            vector<Node*> nodeList(100);
+            vector<bool> isVisit(100, false);
+            nodeList[0] = root;
+            for (int i = 1; i < 100; i++)
             {
                 nodeList[i] = new Node(i + 1);
             }
 
-            Node* curr_origin = node;
-            for (int i = 0; i < endVal; i++)
-            {
-                for (int j = 0; j < curr_origin->neighbors.size(); j++)
-                {
-                    // visit 여부를 확인해서 추가해야함;;;;;
-                    nodeList[i]->neighbors.push_back(nodeList[curr_origin->neighbors[j]->val - 1]);
-                }
-            }
+            Node* base = node;
+            Node* copy = root;
 
-            delete[] nodeList;
+            DFS(copy, base, nodeList, isVisit);
         }
 
-        return origin;
+        return root;
+    }
+
+    void DFS(Node* copy, Node* base, vector<Node*>& nodeList, vector<bool>& isVisit)
+    {
+        isVisit[copy->val - 1] = true;
+
+        for (int i = 0; i < base->neighbors.size(); i++)
+        {
+            copy->neighbors.push_back(nodeList[base->neighbors[i]->val - 1]);
+        }
+
+        for (int j = 0; j < copy->neighbors.size(); j++)
+        {
+            if (!isVisit[copy->neighbors[j]->val - 1])
+            {
+                DFS(copy->neighbors[j], base->neighbors[j], nodeList, isVisit);
+            }
+        }
     }
 };
-
-int main()
-{
-    Solution s;
-
-    Node* n1 = new Node(1);
-    Node* n2 = new Node(2);
-    Node* n3 = new Node(3);
-
-    n1->neighbors.push_back(n2);
-    n1->neighbors.push_back(n3);
-    n2->neighbors.push_back(n1);
-    n3->neighbors.push_back(n1);
-
-    s.cloneGraph(n1);
-
-    delete n1, n2, n3;
-
-    return 0;
-}
